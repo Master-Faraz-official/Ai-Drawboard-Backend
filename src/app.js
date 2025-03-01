dc5afe3fecdc077204696efeb6fc161142431e83
Faraz Ali <farazofficial9028@gmail.com>
2025-03-01 23:43:12 +0530

    resolved the CORS Error

--- src/app.js
+++ src/app.js
@@ -7,10 +7,17 @@ import userRouter from "./routes/user.routes.js";
 const app = express()
 
 // Implemanting cors configuration
-app.use(cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true
-}))
+// app.use(cors({
+//     origin: process.env.CORS_ORIGIN,
+//     credentials: true
+// }))
+
+app.use(
+    cors({
+        origin: process.env.FRONTEND_URL || "http://localhost:3000", // Change this to match your frontend URL
+        credentials: true, // Allow cookies
+    })
+);
 
 // We are accepting json data and setting limit 
 app.use(express.json({ limit: '50mb' }))
